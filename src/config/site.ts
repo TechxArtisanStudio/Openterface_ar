9f6fbd91726377d43307ae51c07d57c6667236a3
Billy Wang <billy@techxartisan.com>
2026-05-26 00:34:05 +0800

    Add tr locale to cross-subdomain hreflang cluster and linker domains.

--- src/config/site.ts
+++ src/config/site.ts
@@ -39,6 +39,7 @@ export const siteConfig = {
         'hk.openterface.com',
         'ru.openterface.com',
         'ar.openterface.com',
+        'tr.openterface.com',
         'news.openterface.com',
         'docs.openterface.com',
       ],
